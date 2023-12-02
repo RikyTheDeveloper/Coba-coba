e171cb7e5b4cb28bae14f69ae449c92dabb0f986
RikyTheDeveloper <152715922+RikyTheDeveloper@users.noreply.github.com>
2023-12-02 23:16:54 +0700

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,31 @@
+const wrapper = document.querySelector(".wrapper");
+
+const question = document.querySelector(".question");
+
+const gif document.querySelector(".gif");
+
+const yesBtn = document.querySelector(".yes-btn");
+
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+
+question.innerHTML = "MAKACI";
+
+gif.src =
+
+}); "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+
+noBtn.addEventListener("mouseover", () {
+
+const noßtnRect = noBtn.getBoundingClientRect();
+
+const maxX = window.innerWidth noßtnRect.width;
+
+const maxY = window.innerHeight noßtnRect.height;
+
+const randomX = Math.floor(Math.random() * maxx);
+
+const randomy = Math.floor(Math.random() * maxY);
+
+noBtn.style.left = randomX + "px"; noBtn.style.top randomY + "px"; });
